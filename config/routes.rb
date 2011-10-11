43739a67a7f9786f59ae33df7395a67828b958e6
PALMA <leandro.palma@gmail.com>
2011-10-11 23:42:31 +0100

    create store index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Depot::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Depot::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'product#index'
+   root :to => 'store#index'
 
   # See how all your routes lay out with "rake routes"
 
